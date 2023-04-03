9f783d1331ddc57a21f1f0935a3a07b350eb4d69
begaiyma <turdukeeva064@gmail.com>
2023-04-03 15:54:58 +0600

    link to

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <link to ="/">Home</link>
+      <link to = "/phones">Phones</link>
+
     </div>
   );
 }
