0cd754248fa702063c194cc4dceb9679d1aeada4
begaiyma <turdukeeva064@gmail.com>
2023-04-07 14:46:10 +0600

    added Components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,14 @@
 import "./Logo.css";
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 export default function Logo () {
   return (
     <div className="Logo">
+      <NavLink to="logo">
       <img src={logo} alt="logo" className="logo"></img>
+      </NavLink>
+
 
     </div>
   )
