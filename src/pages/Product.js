9f8ac84eb0254e9718d3a0da8e20cefa9b4c27d7
begaiyma <turdukeeva064@gmail.com>
2023-04-17 13:10:53 +0600

    create classname and added atribut

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,23 +1,24 @@
 import { useContext } from "react";
-import { useMatch } from "react-router-dom"
+import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 
-export default function Product () {
-  const {params} = useMatch("/products/:slug");
-  const {products} = useContext(AppContext);
+export default function Product() {
+  const { params } = useMatch("/products/:slug");
+  const { products } = useContext(AppContext);
 
   const product = products.find(product => product.slug === params.slug);
 
-  if (product) {
-    return <NotFound/>
+  if (!product) {
+    return <NotFound />;
   }
-  
+
   return (
-    <div className="Product">
+    <div className="ProduContainer">
       <h1>{product.name}</h1>
-      <img src={product.picture} alt={product.name}/>
-      <span>{product.price}</span>
+      <img src={product.picture} alt={product.name} />
+      <span>{product.price} som</span>
+      <p>{product.description}</p>
     </div>
-  )
+  );
 }
