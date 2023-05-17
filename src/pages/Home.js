10a0c59038579ec26a15b3338538eaff33aafe6a
begaiyma <turdukeeva064@gmail.com>
2023-05-17 12:37:39 +0600

    in third section i added video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,11 @@
 import './Home.css';
 import homeimg from '../assets/bycyclee.jpg';
 
+function plusSlides(n) {
+
+}
+
+
 export default function Home() {
   return (
     <div className='sections'>
@@ -9,13 +14,15 @@ export default function Home() {
         <div className='wrapper'>
           <div className='page-first'>
             <div className='pageone-image'>
-              <img src={homeimg} alt='pageoneimg'></img>
+            <img src={homeimg} alt="homeimg" />
             </div>
             <div className='text-first'>
-            <h2 className="section-title">Add some sunshine to their day with a cheery summer flower bouquet.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque cum, perspiciatis a illo veniam ratione expedita assumenda laboriosam</p>
+              <h2 className='section-title'>Add some sunshine  to their day with a cheery summer flower bouguet </h2>
+              <p>
+                lorem ipsum dolor sit amet consectetur adipisicing elit. Cumgue cum, perspiciatis a illo veniam ratione expedita  assumenda laboriosam
+              </p>
             </div>
-          </div>
+          </div>    
         </div>
       </section>
 
@@ -28,8 +35,10 @@ export default function Home() {
 
       <section>
         <div className="wrapper">
-          <h2 className="section-title">FLOWER DELIVERY TO PUT A SMILE ON THEIR FACE</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque cum, perspiciatis a illo veniam ratione expedita assumenda laboriosam modi error maiores fugit soluta vitae temporibus voluptatum ducimus culpa similique quaerat?</p>
+          <div className='video'>
+          <iframe  src="https://www.youtube.com/embed/n8T3cmb8kJo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+          </div>
         </div>
       </section>
 
