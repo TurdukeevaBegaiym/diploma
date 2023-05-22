642b15f9215f831fb69becfe01d27e1ff6252d39
begaiyma <turdukeeva064@gmail.com>
2023-05-22 23:21:36 +0600

    produclist  component

--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -15,7 +15,7 @@ export default function ProductList ({category}) {
     <div key={product.id} className="Product">
       <img src = {product.picture} alt={product.name}/>
       <NavLink to={'/products/' + product.slug}>
-        {product.name}
+        {product.name} 
       </NavLink>
       <span>{product.price} $</span>
       <AddToCart product={product}/>
