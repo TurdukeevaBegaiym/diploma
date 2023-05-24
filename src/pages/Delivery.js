2ae1a85b0d10968eefd99ec472be1030d9b6ed76
begaiyma <turdukeeva064@gmail.com>
2023-05-25 02:03:28 +0600

    fixed my error

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,28 +1,10 @@
 
-import React from 'react';
-import Carousel from './Carousel';
-
 export default function Delivery() {
-  const items = [
-    {
-      imageUrl: 'https://example.com/image1.jpg',
-      title: 'Item 1',
-    },
-    {
-      imageUrl: 'https://example.com/image2.jpg',
-      title: 'Item 2',
-    },
-    {
-      imageUrl: 'https://example.com/image3.jpg',
-      title: 'Item 3',
-    },
-  ];
-
   return (
     <div className="Delivery">
       <h1>Delivery</h1>
       <p>This is my home page</p>
-      <Carousel items={items} />
+     
     </div>
   );
 }
