093244e5a8cdbd8128211f8fead5b48a1f22460d
begaiyma <turdukeeva064@gmail.com>
2023-05-25 02:21:22 +0600

    fixed  error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,10 +47,7 @@ export default function Home() {
 
       <section className='dioganal'>
         <div className="wrapper">
-          <div className='video'>
-          <iframe  src="https://www.youtube.com/embed/n8T3cmb8kJo" autoPlay title="YouTube video player" frameBorder="0" allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen=""></iframe>
-
-          </div>
+          
         </div>
       </section>
 
