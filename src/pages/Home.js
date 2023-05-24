8442243530530114d21f24eeb0f9d28ef57ecb64
begaiyma <turdukeeva064@gmail.com>
2023-05-25 02:04:54 +0600

    in section dioganal added new code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 
 import './Home.css';
 import homeimg from '../assets/bycyclee.jpg';
+import make from '../assets/make.png'
+import take from '../assets/take.png'
 
 
 export default function Home() {
@@ -23,16 +25,30 @@ export default function Home() {
       </section>
 
       <section className='diagonal'>
-        <div className='wrapper'>
-          <h2 className="section-title">Diagonal</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque cum, perspiciatis a illo veniam ratione expedita assumenda laboriosam modi error maiores fugit soluta vitae temporibus voluptatum ducimus culpa similique quaerat?</p>
+        <div className='wrapper wrap'>
+          <h2 className="section-title">Why a BloomsyBox Subscription</h2>
+          <div className='three'>
+          <div className='make'>
+          <img src={make} alt="homeimg"  className='diogana'/>
+          <h1>Make it yours</h1>
+          <p>Choose your flowers, tell us where you want ‘em delivered, and get ready for a hand-wrapped delivery of the freshest seasonal flowers our Rainforest Alliance certified farms have to offer.</p>
+          </div>
+
+          <div className='take'>
+          <img src={take} alt="hom"  className='diogana'/>
+          <h1>On your own terms</h1>
+          <p>Want flowers delivered weekly? Monthly? Twice monthly? It’s all up to you! You can fully customize your subscription plan according to your needs at any time.</p>
+          </div>
+
+        
+        </div>
         </div>
       </section>
 
       <section className='dioganal'>
         <div className="wrapper">
           <div className='video'>
-          <iframe  src="https://www.youtube.com/embed/n8T3cmb8kJo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen=""></iframe>
+          <iframe  src="https://www.youtube.com/embed/n8T3cmb8kJo" autoplay title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen=""></iframe>
 
           </div>
         </div>
