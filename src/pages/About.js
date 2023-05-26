c2ea6644cc7a92c218d2d026166fb27aa452e998
begaiyma <turdukeeva064@gmail.com>
2023-05-26 14:27:22 +0600

    added tw classname in about class

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,21 @@
 import  './About.css'
+import girlflower from '../assets/gilrflower.png'
+import Flowersgirl from '../assets/flowersgirl.avif'
 export default function About () {
   return (
     <div className="About">
-      <div className="card">
-        <div className='card-content'>
-        <h2 className="card-title">Something about us
-        </h2>
-        <p className="card-body">Lorem imsum kjhxg bxhvsgbkjbhs jxbshvhg </p>
-      </div>
-      </div>
+     <div className='aboutus'>
+     <img src={girlflower} alt="girlflower" />
+     <h2>Kimberly Gilroy</h2>
+     <p>Founder</p>
+     </div>
+
+
+     <div className='aboutuss'>
+      <h2>A diverse team <br/> founded  by females</h2>
+      <p>Our intenational network of award-winning developers <br/>  writers, desigeners, and photographers allow us to curate <br/>  the best possible team for each project. We're passionate <br/>  about what we do and the relationships we foster</p>
+     <img src={Flowersgirl} alt="flowersgirl" />
+     </div>
     </div>
   );
 }
